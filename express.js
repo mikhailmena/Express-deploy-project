24487a26e63b24c896566fa644ac86f80d3ccc4e
mikhailmena <marcmmenard@gmail.com>
2022-10-07 12:22:26 -0600

    'messed up'

---
+++ express.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const {Client} = require('pg');
+app.use(cors())
+app.use(bodyParser())
+app.use(express.static('public'))
+const client = new Client({
+    connectionString:"postgres://postgres:docker@localhost:5432/memo_db"
+});
+client.connect();
+
+app.get('/api/memo', (req, res) => {
+    client.query('SELECT * FROM memo_table').then((result) => {
+       res.setHeader('Content-Type', 'application/json');
+       res.send(result.rows);  
+    })
+})
+
+app.post('/api/memo', (req, res) => {
+    let newComment = req.body
+    client.query("INSERT INTO memo_table(description) VALUES ($1);",[newComment.description]).then((data)=>{
+        res.send(newComment)
+        
+    });
+});
+
+app.delete('/api/memo',(req, res)=>{
+    let id = req.body.id; 
+    client.query('DELETE FROM memo_table WHERE memo_id = $1',[id]).then((data) =>{
+        
+        res.send()
+    })
+});
+
+
+app.listen(PORT, () =>{
+    console.log('Listening on port: ', PORT);
+});
---
+++ public/memo.js
@@ -0,0 +1,34 @@
+// const { response } = require("express");
+
+$.get('http://localhost:8000/api/memo', (data) => {
+    console.log(data)
+    // let $newDiv ' $'
+    for (let i =0; i < data.length; i++){
+        $('#display').prepend(data[i].description)
+    }
+     //prepends data from get call to display ---- I need to make a for loop to display all of it
+});
+
+const formSub = document.querySelector('.form'); //selects the form and puts it in a variable
+
+// post from submit form
+formSub.addEventListener('submit', event => { //adds event listener
+    event.preventDefault(); 
+    const formData = new FormData(formSub); //takes all data from form
+    const formInput = Object.fromEntries(formData); //puts that dat into a 
+    
+    fetch('http://localhost:8000/api/memo', {method: 'POST', headers: {'Content-Type':'application/json'}
+    ,body: JSON.stringify(formInput)}) //turns the form data into a string
+    .then(response => {
+    $('#display').append(formInput.description) //appends stringified form data to display div
+    
+    console.log(formInput)
+    // location.reload()
+    
+    
+
+})
+    
+
+})
+
