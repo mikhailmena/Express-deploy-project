eea57273d3e284e3a28717875b8a73d2ac1f011b
mikhailmena <marcmmenard@gmail.com>
2022-10-07 20:11:53 -0600

    adding commentsdb and adding node to dependencies

--- public/memo.js
+++ public/memo.js
@@ -1,13 +1,13 @@
 // const { response } = require("express");
 let ApiURL = ENV == "dev" ? "http://localhost:8000" : "https://express-ui-0iip.onrender.com";
-$.get(`${ApiURL}/api/comments`), (data) => {
+$.get({'https://express-ui-0iip.onrender.com'}, (data) => {
     console.log(data)
     // let $newDiv ' $'
     for (let i =0; i < data.length; i++){
         $('#display').prepend(data[i].description)
     }
      //prepends data from get call to display ---- I need to make a for loop to display all of it
-};
+});
 
 const formSub = document.querySelector('.form'); //selects the form and puts it in a variable
 
@@ -17,7 +17,7 @@ formSub.addEventListener('submit', event => { //adds event listener
     const formData = new FormData(formSub); //takes all data from form
     const formInput = Object.fromEntries(formData); //puts that dat into a 
     
-    fetch(`${ApiURL}/api/comments`, {method: 'POST', headers: {'Content-Type':'application/json'}
+    fetch('https://express-ui-0iip.onrender.com', {method: 'POST', headers: {'Content-Type':'application/json'}
     ,body: JSON.stringify(formInput)}) //turns the form data into a string
     .then(response => {
     $('#display').append(formInput.description) //appends stringified form data to display div
